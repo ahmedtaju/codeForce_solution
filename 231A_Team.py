8af0ac231504c7c6a543c673484ee485518fbd43
ahmedtaju <151705759+ahmedtaju@users.noreply.github.com>
2024-11-17 03:15:20 +0300

    Update 231A_Team.py

--- 231A_Team.py
+++ 231A_Team.py
@@ -1,12 +1,12 @@
 n=int(input())
 tries=[]
-Sum=0
+_sum=0
 for i in range(n):
     p,v,t=map(int,input().split())
     tries.append((p,v,t))
     if sum(tries[i])>1:
-        Sum +=1
+        _sum +=1
     else:
         continue
-print(Sum)
+print(_sum)
 
