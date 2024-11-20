7f020cdbf8a65d5d052a049125742599d1b45869
ahmedtaju <151705759+ahmedtaju@users.noreply.github.com>
2024-11-20 17:03:33 +0300

    Update Rook.py

--- Rook.py
+++ Rook.py
@@ -4,8 +4,8 @@ for i in range(t):
     a=list(s)
     b=["a","b","c","d","e","f","g","h"]
     for i in b:
-        if not i==a[0]:
+        if i!=a[0]:
             print(i+a[1])
     for j in range(1,9):
-        if not int(a[1])==j:
+        if int(a[1])!=j:
             print(a[0]+str(j))
